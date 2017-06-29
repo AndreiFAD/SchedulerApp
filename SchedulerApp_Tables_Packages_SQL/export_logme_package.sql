9bf13a1143fdbdcc85b499661d41a2062ae57853
Fekete András Demeter <andrei.dumitru.fechete@gmail.com>
2017-06-30 00:23:25 +0200

    Update

--- SchedulerApp_Tables_Packages_SQL/export_logme_package.sql
+++ SchedulerApp_Tables_Packages_SQL/export_logme_package.sql
@@ -1,4 +1,4 @@
-create or replace PACKAGE logme_package
+CREATE OR REPLACE PACKAGE REPAPP.logme_package
   IS
   
   PROCEDURE mail_send
@@ -8,31 +8,31 @@ create or replace PACKAGE logme_package
       v_bcc IN VARCHAR2,
       v_subject IN VARCHAR2,
       v_message IN clob
-      )
+      );
   
    PROCEDURE log_time_send
-     ( v_logid IN VARCHAR2,
+   	 ( v_logid IN VARCHAR2,
        v_runtime OUT VARCHAR2);
        
    PROCEDURE new_process
      ( v_id OUT NUMBER,
        v_job_no IN NUMBER,
-       job_name IN VARCHAR2(50 BYTE),
-       job_info IN VARCHAR2(1000 BYTE) );
+       job_name IN VARCHAR2,
+       job_info IN VARCHAR2);
 
    PROCEDURE close_process
      ( v_id IN NUMBER,
-       v_job_info IN VARCHAR2(1000 BYTE),
-       v_error VARCHAR2(512 BYTE) default '' );
+       v_job_info IN VARCHAR2,
+       v_error VARCHAR2 default '' );
 
    PROCEDURE new_subprocess
      ( v_job_id IN NUMBER,
        job_sub_id IN NUMBER,
-       job_info IN VARCHAR2(1000 BYTE) );
+       job_info IN VARCHAR2 );
 
    PROCEDURE update_process
      ( v_job_id IN NUMBER,
        v_job_sub_id IN NUMBER,
-       v_job_progress_percent IN NUMBER(3,0) default -1,
-       v_job_info IN VARCHAR2(1000 BYTE) default 'NULL' );
+       v_job_progress_percent IN NUMBER default -1,
+       v_job_info IN VARCHAR2 default 'NULL' );
 END;
